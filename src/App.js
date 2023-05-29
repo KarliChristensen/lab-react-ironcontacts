e359f8dbb6a42bd944d0682f5d41d23020b5d2cb
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-05-29 21:21:04 +0200

    Bonus

--- src/App.js
+++ src/App.js
@@ -45,45 +45,71 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Ironhack Contacts</h1>
-      <button onClick={findRandomCeleb}>Add Random celeb</button>
-      <button onClick={sortByName}>Sort by name</button>
-      <button onClick={sortByPopularity}>Sort by popularity</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Won Oscar</th>
-            <th>Won Emmy</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {state.map((contact, index) => {
-            return (
-              <tr key={index}>
-                <td>
-                  <img
-                    src={contact.pictureUrl}
-                    style={{ width: "50px" }}
-                    alt="Celebrity"
-                  />
-                </td>
-                <td>{contact.name}</td>
-                <td>{contact.popularity}</td>
-                <td>{contact.wonOscar ? "🏆" : null}</td>
-                <td>{contact.wonEmmy ? "🏆" : null}</td>
-                <td>
-                  <button onClick={deleteCeleb}>Delete</button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-2xl">
+        <h1 className="text-3xl font-bold mb-4">Ironhack Contacts</h1>
+        <button
+          onClick={findRandomCeleb}
+          className="px-4 py-2 mt-4 mr-2 text-white bg-blue-500 rounded"
+        >
+          Add Random celeb
+        </button>
+        <button
+          onClick={sortByName}
+          className="px-4 py-2 mt-4 mr-2 text-white bg-blue-500 rounded"
+        >
+          Sort by name
+        </button>
+        <button
+          onClick={sortByPopularity}
+          className="px-4 py-2 mt-4 mr-2 text-white bg-blue-500 rounded"
+        >
+          Sort by popularity
+        </button>
+        <table className="w-full bg-white border border-gray-200 rounded-lg">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Picture</th>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Popularity</th>
+              <th className="px-4 py-2">Won Oscar</th>
+              <th className="px-4 py-2">Won Emmy</th>
+              <th className="px-4 py-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.map((contact, index) => {
+              return (
+                <tr key={index}>
+                  <td className="px-4 py-2">
+                    <img
+                      src={contact.pictureUrl}
+                      className="rounded-full border border-gray-200 w-12 h-12 object-cover"
+                      alt="Celebrity"
+                    />
+                  </td>
+                  <td className="px-4 py-2">{contact.name}</td>
+                  <td className="px-4 py-2">{contact.popularity}</td>
+                  <td className="px-4 py-2">
+                    {contact.wonOscar ? "🏆" : null}
+                  </td>
+                  <td className="px-4 py-2">
+                    {contact.wonEmmy ? "🏆" : null}
+                  </td>
+                  <td className="px-4 py-2">
+                    <button
+                      onClick={deleteCeleb}
+                      className="px-4 py-2 text-white bg-red-500 rounded"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
